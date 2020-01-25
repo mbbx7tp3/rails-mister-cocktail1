9053deab542694d1201ac194e9fe954fc7a5162b
Tom Poulter <tom.poulter.1@outlook.com>
2020-01-25 14:12:52 +0000

    nearly there with models

---
+++ app/models/dose.rb
@@ -0,0 +1,6 @@
+class Dose < ApplicationRecord
+  belongs_to :cocktail
+  belongs_to :ingredient
+  validates :description, uniqueness: true, presence: true
+  validates_uniqueness_of :ingredient scope: :cocktail
+end
