a3f022935bfd4b22d6248c07e1c2c11b3a320784
Tom Poulter <tom.poulter.1@outlook.com>
2020-01-28 19:42:03 +0000

    nearly there with my doses

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  resources :cocktails
+  root to: "cocktails#inde"
+  resources :cocktails, only: [ :new, :create, :show, :index] do
+    resources :doses, only: [ :new, :create]
+  end
+  resources :doses, only: [:destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,14 @@
+lemon = Ingredient.create!(name: "lemon")
+ice = Ingredient.create!(name: "ice")
+applejack = Ingredient.create!(name: "Applejack")
+applebrandy = Ingredient.create!(name: "Apple brandy")
+creme = Ingredient.create!(name: "Creme de Cassis")
+bloody = Cocktail.create!(name: "Bloddy Mary")
+vodka = Cocktail.create!(name: "Vodka Red bull")
+jager = Cocktail.create!(name: "Jager Bomb")
 
-Ingredient.create(name: "lemon")
-Ingredient.create(name: "ice")
-Ingredient.create(name: "mint leaves")
-Ingredient.create(name: "Applejack")
-Ingredient.create(name: "Apple brandy")
-Ingredient.create(name: "Creme de Cassis")
-Cocktail.create(name: "Bloddy Mary")
-Cocktail.create(name: "Vodka Red bull")
-Cocktail.create(name: "Jager Bomb")
+Dose.create!(description: "5cl", cocktail: bloody, ingredient: lemon)
+Dose.create!(description: "5cl", cocktail: vodka, ingredient: lemon)
 
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
